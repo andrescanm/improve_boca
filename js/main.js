71c4b6a92bf16004fa82da0043f6e27bb99c6f0d
andrescanm <andres.co@hotmail.com>
2018-09-30 18:26:13 -0500

    Avances configuración contenido módulos

--- js/main.js
+++ js/main.js
@@ -10,13 +10,51 @@ Código JavaScript original BOCA
  (at your option) any later version. A copy of the  
  license can be found with this software or at http://www.gnu.org/licenses/
  ---------------------------------------------------------------------- 
-*/
-function computeHASH()
-{
-	var userHASH, passHASH;
-	userHASH = document.form1.name.value;
-	passHASH = js_myhash(js_myhash(document.form1.password.value)+'5d4574nnnjutrkert0buu9ukf5');
-	document.form1.name.value = '';
-	document.form1.password.value = '                                                                                 ';
-	document.location = 'index.php?name='+userHASH+'&password='+passHASH;
+ */
+ /********************************************************************/
+ /*Invocada en: login.php*/
+ /********************************************************************/
+ function computeHASH()
+ {
+ 	var userHASH, passHASH;
+ 	userHASH = document.form1.name.value;
+ 	passHASH = js_myhash(js_myhash(document.form1.password.value)+'5d4574nnnjutrkert0buu9ukf5');
+ 	document.form1.name.value = '';
+ 	document.form1.password.value = '                                                                                 ';
+ 	document.location = 'index.php?name='+userHASH+'&password='+passHASH;
+ }
+
+ /********************************************************************/
+ /*Invocada en: user.php*/
+ /********************************************************************/
+ function conf2(url) {
+ 	if (confirm("Are you sure?")) {
+ 		document.location=url;
+ 	} else {
+ 		document.location='user.php';
+ 	}
+ }
+
+ function computeHASH()
+ {
+ 	document.form3.passwordn1.value = bighexsoma(js_myhash(document.form3.passwordn1.value),js_myhash(document.form3.passwordo.value));
+ 	document.form3.passwordn2.value = bighexsoma(js_myhash(document.form3.passwordn2.value),js_myhash(document.form3.passwordo.value));
+ 	document.form3.passwordo.value = js_myhash(js_myhash(document.form3.passwordo.value)+'thhfqie4glc7uqgssmekjeuen5');
+ }
+
+//Funciones que aplican al formulario de creación de usuarios
+function conf() {
+	if (confirm("Confirm?")) {
+		document.form1.confirmation.value='confirm';
+	}
+}
+
+function conf3() {
+	computeHASH();
+	if (confirm("Confirm?")) {
+		document.form3.confirmation.value='confirm';
+	}
+}
+function conf5() {
+	document.form3.confirmation.value='noconfirm';
 }
